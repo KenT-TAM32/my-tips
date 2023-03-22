40b3cf87296842b5dce395991d4fe2098393f54c
kenT <tam.store32@gmail.com>
2023-03-22 18:59:07 +0900

    add show gravatar user

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "/contact", to: "static_pages#contact"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "static_pages#home"
+  resources :users
   
   # Defines the root path route ("/")
   # root "articles#index"
