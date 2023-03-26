11fbbec5b9e516134100876e95ec7e237a80b3c6
kenT <tam.store32@gmail.com>
2023-03-26 12:21:01 +0900

    add login_form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get "/signup", to: "users#new"
+  # get 'session/new'
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   # get 'static_pages/home'
   get "/help", to: "static_pages#help"
   get "/about", to: "static_pages#about"
