61031416b666b08c85b1454dca8fd7ad0658456f
kenT <tam.store32@gmail.com>
2023-03-30 17:09:18 +0900

    modified authorization

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,19 @@
 class SessionsController < ApplicationController
 
   def new
+    debugger
   end
 
   def create
+    #9章演習問題にてuserを@userに変更（10章以降の本分コードと差異有）
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user&.authenticate(params[:session][:password])
+      forwarding_url = session[:forwarding_url]
       reset_session           # 必ず記述
       params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
       # remember user
       log_in @user
-      redirect_to @user
+      redirect_to forwarding_url || @user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new', status: :unprocessable_entity
